2bccb5cfc172fe7126a9fdd7d152870dcbcc7a7c
sampath1901 <60257678+sampath1901@users.noreply.github.com>
2020-07-20 06:45:49 -0700

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int prime(int);
+int i;
+int main()
+{
+
+    int n,primeN;
+    printf(" Input any positive number : ");
+    scanf("%d",&n);
+    i = n/2;
+    primeN = prime(n);//call the function checkForPrime
+   if(primeN==1)
+        printf(" The number %d is a prime number. \n\n",n);
+   else
+      printf(" The number %d is not a prime number. \nn",n);
+   return 0;
+}
+
+int prime(int n)
+{
+    if(i==1)
+    {
+        return 1;
+    }
+    else if(n %i==0)
+    {
+         return 0;
+    }     
+    else
+       {
+         i = i-1; 
+         prime(n);
+      }
+}
+
